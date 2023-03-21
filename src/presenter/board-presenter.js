84d931451b496bdb5c6783323545c8c50dd0971a
Вера <Vera.Iagodkina@urfu.me>
2023-03-21 16:33:43 +0500

    part 1

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,23 +5,69 @@ import PreviewPointView from '../view/preview-point-view.js';
 import { render } from '../render.js';
 
 export default class BoardPresenter {
+  #boardComponent = null;
+  #boardContainer = null;
+  #pointsModel = null;
+  #boardPoints = null;
+  #destinations = null;
+  #offers = null;
+
   constructor(boardContainer) {
-    this.boardComponent = new EventsView();
-    this.boardContainer = boardContainer;
+    this.#boardComponent = new EventsView();
+    this.#boardContainer = boardContainer;
   }
 
   init(pointsModel) {
-    this.pointsModel = pointsModel;
-    this.boardPoints = [...this.pointsModel.getPoints()];
-    this.destinations = [...this.pointsModel.getDestinations()];
-    this.offers = [...this.pointsModel.getOffers()];
+    this.#pointsModel = pointsModel;
+    this.#boardPoints = [...this.#pointsModel.points];
+    this.#destinations = [...this.#pointsModel.destinations];
+    this.#offers = [...this.#pointsModel.offers];
 
-    render(new SortView(), this.boardContainer);
-    render(this.boardComponent, this.boardContainer);
-    render(new EditFormView(this.boardPoints[0], this.destinations, this.offers), this.boardComponent.getElement());
+    render(new SortView(), this.#boardContainer);
+    render(this.#boardComponent, this.#boardContainer);
 
-    for (const point of this.boardPoints){
-      render(new PreviewPointView(point, this.destinations, this.offers), this.boardComponent.getElement());
+    for (const point of this.#boardPoints){
+      this.#renderPoint(point);
     }
   }
+
+  #renderPoint = (point) => {
+    const pointComponent = new PreviewPointView(point, this.#destinations, this.#offers);
+    const pointEditComponent = new EditFormView(point, this.#destinations, this.#offers);
+
+    const replacePointToEditForm = () => {
+      this.#boardComponent.element.replaceChild(pointEditComponent.element, pointComponent.element);
+    };
+
+    const replaceEditFormToPoint = () => {
+      this.#boardComponent.element.replaceChild(pointComponent.element, pointEditComponent.element);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replacePointToEditForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', (evt) => {
+      evt.preventDefault();
+      replaceEditFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    pointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceEditFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(pointComponent, this.#boardComponent.element);
+  };
 }
--- src/view/create-form-view.js
+++ src/view/create-form-view.js
@@ -145,19 +145,20 @@ const createCreateFormTemplate = () => (
 );
 
 export default class CreateFormView {
-  getTemplate () {
+  #element = null;
+  get template () {
     return createCreateFormTemplate();
   }
 
-  getElement() {
-    if (!this.element){
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element){
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
